86bf3a4067ecff18100cbb48a113f2b6a97fbb57
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-03-17 16:35:59 +0000

    Generate an m3u from template with the correct abspath

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,32 @@
     https://pytest.org/latest/plugins.html
 """
 
-# import pytest
+import pytest
+import pathlib
+
+
+test_m3utemplate = pathlib.Path("./tests/test_files/test.template")
+test_m3upath = pathlib.Path("./tests/test_files/test.m3u")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def m3upath(request):
+    return test_m3upath
+
+
+def prepare_m3u(file_name: pathlib.Path, output_name: pathlib.Path):
+    with open(file_name, "r") as file:
+        data = file.read()
+
+    # parents[0] should give the absolute path-directory of the file
+    abs_path = file_name.absolute().parents[0]
+    data = data.replace(r"{full_path}", str(abs_path))
+
+    with open(output_name, "w") as file:
+        file.write(data)
+
+    return output_name
+
+
+if not test_m3upath.exists() and test_m3utemplate.exists():
+    prepare_m3u(test_m3utemplate, test_m3upath)
